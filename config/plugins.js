f895f6a219373e22e77f683583c042faaa0c87f2
Luis Carlos Charris Molina <luis.carlos.charris@gmail.com>
2025-01-15 06:04:16 -0500

    feat: set cloud flare

--- config/plugins.js
+++ config/plugins.js
@@ -6,14 +6,20 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "strapi-provider-cloudflare-r2",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        accessKeyId: env("R2_ACCESS_KEY_ID"),
+        secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
+        endpoint: `https://${env("R2_ACCOUNT_ID")}.r2.cloudflarestorage.com`,
+        params: {
+          Bucket: env("R2_BUCKET"),
+        },
+        cloudflarePublicAccessUrl: env("R2_PUBLIC_URL"),
+        pool: false,
       },
       actionOptions: {
         upload: {},
+        uploadStream: {},
         delete: {},
       },
     },
