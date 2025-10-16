3c08eb7f64ab973c7ed5ef2a8684db3cc23c8077
d3rk4r <alexdiazber74@gmail.com>
2025-10-16 11:53:31 -0500

    Implemented Strapi blog data model: added blog-category (code, label, icon) and blog-post (externalId, title, excerpt, content, featuredImage, author, category, tags, readTime) with relations. Created blog.author component and replaced string image fields with media (featuredImage, avatar). Added controllers, routes, and services scaffolding. Ready to seed categories and manage posts with images via the admin.

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface BlogAuthor extends Schema.Component {
+  collectionName: 'components_blog_authors';
+  info: {
+    displayName: 'author';
+  };
+  attributes: {
+    name: Attribute.String & Attribute.Required;
+    avatar: Attribute.Media;
+    role: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'blog.author': BlogAuthor;
+    }
+  }
 }
