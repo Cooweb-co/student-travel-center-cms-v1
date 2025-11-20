b7a494362e7e46d2dc16331051b79de8799b6104
Brandon Corpas <corpasdev@gmail.com>
2025-11-20 13:26:41 -0500

    feat: add automatic author avatar population to blog-post queries
    - Created ensureAuthorAvatarPopulate helper to automatically include author avatar in populate queries
    - Override find and findOne methods to ensure author.avatar is always populated
    - Safely handle different populate query formats (object, array, undefined)

--- src/api/blog-post/controllers/blog-post.js
+++ src/api/blog-post/controllers/blog-post.js
@@ -6,4 +6,45 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::blog-post.blog-post');
+const ensureAuthorAvatarPopulate = (ctx) => {
+  const populate = ctx.query?.populate;
+
+  const normalizedPopulate =
+    populate && typeof populate === 'object' && !Array.isArray(populate)
+      ? { ...populate }
+      : {};
+
+  const authorConfig =
+    normalizedPopulate.author && typeof normalizedPopulate.author === 'object' && !Array.isArray(normalizedPopulate.author)
+      ? { ...normalizedPopulate.author }
+      : {};
+
+  const authorPopulate =
+    authorConfig.populate && typeof authorConfig.populate === 'object' && !Array.isArray(authorConfig.populate)
+      ? { ...authorConfig.populate }
+      : {};
+
+  authorPopulate.avatar = true;
+
+  normalizedPopulate.author = {
+    ...authorConfig,
+    populate: authorPopulate,
+  };
+
+  ctx.query = {
+    ...ctx.query,
+    populate: normalizedPopulate,
+  };
+};
+
+module.exports = createCoreController('api::blog-post.blog-post', ({ strapi }) => ({
+  async find(ctx) {
+    ensureAuthorAvatarPopulate(ctx);
+    return await super.find(ctx);
+  },
+
+  async findOne(ctx) {
+    ensureAuthorAvatarPopulate(ctx);
+    return await super.findOne(ctx);
+  },
+}));
